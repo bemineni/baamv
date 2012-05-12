e840aa40d5b046c1ecff4957b742a0ebd06eb4a3
Srikanth Bemineni <srikanth.bemineni@gmail.com>
2012-05-12 09:36:26 -0500

    Initial version of baamv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baamv::Application.config.secret_token = '9fe4f1f5159e1a19a9817c0608483af55fbd9f3067912b49f3c08a7ba00ec471c1cf43aec46e6874ed86d6ac86c267c09875d6fb1c723f31f4aff6f243b959a8'
