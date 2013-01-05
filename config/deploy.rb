45e78c325c8f42f584db67feb8512ba1e5153c55
Srikanth Bemineni <srikanth.bemineni@gmail.com>
2013-01-05 15:28:59 -0600

    Capistrano changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,9 @@
 
+require 'capistrano'
 require "bundler/capistrano"
 # Load RVM's capistrano plugin.    
 require "rvm/capistrano"
+require 'capistrano/cli'
 
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 set :rvm_ruby_string,"1.9.3"
@@ -11,9 +13,14 @@ set :scm, :git
 set :repository,  "git@github.com:bemineni/baamv.git"
 set :branch, "master"
 set :user , "iamzero"
+set :rvm_type, :user
 set :scm_passphrase, "srikanth"  # The deploy user's password
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :rails_env, :production
+set :unicorn_binary, "unicorn"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -32,15 +39,31 @@ def remote_file_exists?(full_path)
 end
 
 
+def process_exists?(pid_file)
+    capture("ps -p $(cat #{pid_file}) ; true").strip.split("\n").size == 2
+end
+
+
 
 namespace :baamv do
 
 	task :default, :roles => :app do
 		deploy.update
-		run "ls g bootstrap:install -f for boot strap"
-		run "bundle exec unicorn_rails -c $unicornFile -E production"
-		run "sudo service nginx restart"
+		
 	end
+
+    desc "Install Boot strap"
+	task :bootstrap , :roles => :app do
+		run "cd #{deploy_to}/current/ ; rails g  bootstrap:install -f"
+	end
+
+	desc "Start nginx server"
+	task :nginx_restart , :roles => :app do
+		set :sudo_password, proc{ Capistrano::CLI.password_prompt("Type your sudo password #{user}: ") }
+#		run "rvmsudo service nginx restart"
+		run "/sbin/service --status-all"
+	end
+
 	desc "Shows all the files in the directory"
 	task :show , :roles => :app do 
 		run "ls -al"
@@ -59,6 +82,47 @@ namespace :baamv do
 	end
 
 
+end
+
+
+
+
+namespace :unicorn do
+
+	task :start, :roles => :app, :except => { :no_release => true } do
+
+		run "cd #{current_path} && bundle exec #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+
+	end
+
+	task :stop, :roles => :app, :except => { :no_release => true } do
+
+		run "kill `cat #{unicorn_pid}`"
+
+	end
+
+	task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+
+		run "#kill -s QUIT `cat #{unicorn_pid}`"
+
+	end
+
+	task :reload, :roles => :app, :except => { :no_release => true } do
+
+		run "#kill -s USR2 `cat #{unicorn_pid}`"
+
+	end
+
+	task :restart, :roles => :app, :except => { :no_release => true } do
+
+		if remote_file_exists? "#{unicorn_pid}"
+			if process_exists? "#{unicorn_pid}"
+				stop
+			end
+		end
+
+		start
+	end
 end
 
 # if you want to clean up old releases on each deploy uncomment this:
